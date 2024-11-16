4d60719039886d0e978188884bc4da86a5e78db6
Abdurazak <abdurazak.awil1@gmail.com>
2024-11-16 07:59:20 -0500

    feat: add endpoint to get book reviews by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -71,8 +71,15 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+    const { isbn } = req.params;
+
+    const book = booksArr.find((book) => book.isbn === isbn);
+  
+    if (book) {
+      return res.status(200).send(JSON.stringify(book.reviews, null, 2));
+    }
+  
+    return res.status(404).send(`Book with isbn "${isbn}" not found.`);
 });
 
 module.exports.general = public_users;
